420e8aea006e9c8850ab343efa1a7286b1d06cde
Konrad Mikucki <komik966@gmail.com>
2021-06-15 10:32:03 +0200

    create log table test

---
+++ Makefile
@@ -0,0 +1,10 @@
+.PHONY: start-db check-fmt test
+
+start-db:
+	docker run -e POSTGRES_PASSWORD=dbmigrat -e POSTGRES_USER=dbmigrat -d -p 5432:5432 postgres:13.3
+
+check-fmt:
+	gofmt -d .
+
+test:
+	go test
--- dbmigrat_test.go
+++ dbmigrat_test.go
@@ -1,1 +1,36 @@
 package dbmigrat
+
+import (
+	"database/sql"
+	_ "github.com/lib/pq"
+	"github.com/stretchr/testify/assert"
+	"log"
+	"os"
+	"testing"
+)
+
+var db *sql.DB
+
+func TestMain(m *testing.M) {
+	_db, err := sql.Open("postgres", "postgres://dbmigrat:dbmigrat@localhost:5432/dbmigrat?sslmode=disable")
+	db = _db
+	if err != nil {
+		log.Fatalln(err)
+	}
+	os.Exit(m.Run())
+}
+
+func TestCreateLogTable(t *testing.T) {
+	resetDB()
+	// # Create table when it not exists
+	assert.NoError(t, CreateLogTable(db))
+	// # Try to create table when it exists
+	assert.NoError(t, CreateLogTable(db))
+}
+
+func resetDB() {
+	_, err := db.Exec(`drop schema if exists public cascade;create schema public`)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
